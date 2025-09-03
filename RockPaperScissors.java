34ddcdf9586eeb61b0e260d96af7df249f151db7
sarah_deschamps <deschamps.sias@gmail.com>
2025-09-03 15:50:05 -0500

    Rock

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class RockPaperScissors {
     
+  public static void main() {
+    Scanner scan = new Scanner(System.in);
+
+
+  }
+
+  public static String rockPaperScissors(int player) {
+
+    Random rand = new Random();
+    String[] choices = {"Rock", "Rock", "Paper", "Scissor"};
+    String choice = choices[(rand.nextInt(5))];
+    return "";
+  }
 }
