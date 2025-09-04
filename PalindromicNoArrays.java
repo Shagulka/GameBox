a916bfac787f7bff0c54348ee21fb9b05c83a566
sarah_deschamps <deschamps.sias@gmail.com>
2025-09-04 17:10:48 -0500

    Implement palindrome and begin other files

--- PalindromicNoArrays.java
+++ PalindromicNoArrays.java
@@ -1,3 +1,31 @@
+import java.util.Scanner;
+
 public class PalindromicNoArrays {
     
+  public static void main() {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Welcome to 'IS THAT A PALINDROME'!!! (with no arrays)");
+    System.out.println("Please type a number, and we will determine (with NO ARRAYS) if this is a palindrome");
+    int num = scan.nextInt();
+    System.out.println(palindrome(num));
+    scan.close();
+  }
+
+  private static String palindrome(int num) {
+    // create a reversed version of num
+    int reversed = 0;
+    // we will divide i by 10 every time
+    int i = num;
+    // we will use this while loop to create reversed
+    while (i > 0) {
+      reversed += i % 10;
+      reversed *= 10;
+      i /= 10;
+    } // while
+    if (reversed == num) {
+      return "This number is a palindrome!";
+    } else {
+      return "This number is not a palindrome :(";
+    }
+  }
 }
