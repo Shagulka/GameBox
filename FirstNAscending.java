65973168a18380f7003924d3099384997ed5013e
sarah_deschamps <deschamps.sias@gmail.com>
2025-09-04 18:08:42 -0500

    Start first n ascending

--- FirstNAscending.java
+++ FirstNAscending.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+
 public class FirstNAscending {
-    
+    // we will print the first n ascending numbers that are multiples of i, j, or both.
+    public static void main() {
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Welcome to 'finding the first n ascending integers that are multiples of i j or both'!!!");
+      System.out.println("(everyones FAVORITE game!!!)");
+      System.out.print("Enter your 'n' (how many numbers in the sequence): ");
+      int n = scan.nextInt();
+      System.out.print("\nEnter your 'i': ");
+      int i = scan.nextInt();
+      System.out.print("\nEnter your 'j': ");
+      int j = scan.nextInt();
+      System.out.println();
+      System.out.println(firstn(n, i, j));
+      scan.close();
+    }
+
+    private static String firstn(int n, int i, int j) {
+      // we want i < j. if that's not the case, we will switch them here.
+      if (i > j) {
+        int curr = i;
+        i = j;
+        j = curr;
+      }
+      return "";
+    }
 }
